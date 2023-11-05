62069b3cc45810d4b01187d729db480a86d79641
Niccolo Dejito <ncdejito@gmail.com>
2023-11-05 23:14:21 +0800

    Add header and build scripts

---
+++ foundations/build-tools/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    int num = 5;
+    int square = num * num;
+    std::cout << "The square of " << num << " is " << square << std::endl;
+    return 0;
+}
---
+++ foundations/header-files/my_math.h
@@ -0,0 +1,7 @@
+#ifndef MY_MATH_H
+#define MY_MATH_H
+
+// Declaration of a simple add function
+int add(int a, int b);
+
+#endif
